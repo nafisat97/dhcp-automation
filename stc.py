e1b908a2b815195c0f0d7d76814941e982408222
Nafisa Tabassum <nafisa.tabassum@telus.com>
2021-11-01 12:03:15 -0600

    better way to create and config dhcp

--- stc.py
+++ stc.py
@@ -1,153 +1,86 @@
-from tkinter import Toplevel
-from stcrestclient import stchttp
-
 import matplotlib.pyplot as plt
 import numpy as np
 import polling
-from datetime import datetime
+import os
+
+
+use_rest_api = True
+server_ip = "127.0.0.1"
+session_name = "test"
+
+if use_rest_api:
+    os.environ["STC_REST_API"] = "1"
+    os.environ["STC_SERVER_ADDRESS"] = server_ip
+    os.environ["STC_SERVER_PORT"] = "8888"
+    os.environ["STC_SESSION_NAME"] = session_name
+    os.environ["STC_SESSION_TERMINATE_ON_DISCONNECT"] = "False"
+    os.environ["EXISTING_SESSION"] = "kill"
+
+    from stcrestclient.stcpythonrest import StcPythonRest as StcPython
 
 
 def main():
-    stc = stchttp.StcHttp(server="127.0.0.1", port="8888")
-    stc.new_session("nafisa", "test", kill_existing=True)
-    stc.join_session("test - nafisa")
-
-    # config_file = "ex.xml"
-
-    # if not os.path.isfile(config_file):
-    #     print("missing file: {}".format(config_file))
-    #     sys.exit(1)
-
     # Spirent Chassis and port details
     chassis_ip = "172.25.205.185"
     port = "11/2"
 
-    project = stc.create("project")
-    port_handle = stc.create("port", under=project)
-    stc.config(port_handle, location="//{}/{}".format(chassis_ip, port))
-
-    deviceList = stc.create(
-        "emulateddevice", under=project, affiliatedPort=port_handle, DeviceCount=1000
-    )
-    ethiiIf = stc.create("EthIIIf", under=deviceList)
-    vlanIF = stc.create("VlanIf", under=deviceList, StackedOn=ethiiIf)
-    ipv4If = stc.create("Ipv4If", under=deviceList, StackedOn=vlanIF)
-    stc.config(deviceList, PrimaryIf=ipv4If, ToplevelIf=ipv4If)
-
-    dhcp_block = stc.create(
-        "dhcpv4blockconfig",
-        under=deviceList,
-        CircuitId="NAFISA-TEST 1/1/1/@x(122,1122)",
-        EnableCircuitId="TRUE",
-        EnableRelayAgent="FALSE",
-        UsesIf=ipv4If,
+    stc = StcPython()
+    project = stc.get("system1", "children-project")
+    resultdict = stc.perform(
+        "CreateLogicalPorts",
+        ParentList=project,
+        CreateClassId="port",
+        PhyClassName="Ethernet10GigFiber",
     )
 
-    # dhcp_results = stc.perform(
-    #     "ResultsSubscribeCommand",
-    #     parent=project,
-    #     resultParent=port_handle,
-    #     resultType="Dhcpv4BlockResults",
-    #     configType="Dhcpv4BlockConfig",
-    #     ViewAttributeList="BindRate",
-    #     interval=1,
-    # )
+    port1 = resultdict["ReturnList"]
+    stc.config(port1, location=f"//{chassis_ip}/{port}")
 
-    # stc.upload(config_file)
-    # data = stc.perform("LoadFromXml", filename=config_file)
-
-    # hProject = stc.get("system1", "children-project")
-    # portList = stc.get(hProject, "children-port")
-    # deviceList = stc.get(hProject, "children-emulateddevice")
-    # print(stc.get(deviceList, "children"))
-    # dhcp_block = stc.get(deviceList, "children-dhcpv4blockconfig")
-
-    sequencer = stc.create("sequencer", under="system1")
-    bind = stc.create("Dhcpv4BindCommand", under=sequencer, BlockList=dhcp_block)
-    bind_wait = stc.create(
-        "Dhcpv4BindWaitCommand",
-        under=sequencer,
-        ObjectList=deviceList,
-        WaitTime=100.0,
-    )
-    release_wait = stc.create(
-        "Dhcpv4ReleaseWaitCommand",
-        under=sequencer,
-        ObjectList=deviceList,
-        WaitTime=100.0,
-    )
-    release = stc.create("Dhcpv4ReleaseCommand", under=sequencer, blockList=dhcp_block)
-
-    stc.perform(
-        "SequencerInsertCommand", commandList=[bind, bind_wait, release, release_wait]
+    # Create DHCP device on port
+    resultdict = stc.perform(
+        "DeviceCreate",
+        ParentList=project,
+        CreateCount=1,
+        DeviceCount=1000,
+        DeviceType="Host",
+        IfStack="Ipv4If VlanIf EthIIIf",
+        IfCount="1 1 1",
+        port=port1,
     )
 
-    # stc.connect(chassis_ip)
-    # stc.perform("reservePort", location="//{}/{}".format(chassis_ip, port))
-    # stc.perform("setupPortMappings")
+    dhcp_device = resultdict["ReturnList"]
 
-    print("Attaching ports")
-    stc.perform("AttachPorts", portList=[port_handle], autoConnect="TRUE")
+    dhcp_enable = stc.perform(
+        "ProtocolCreate",
+        CreateClassId="Dhcpv4BlockConfig",
+        ParentList=dhcp_device,
+        UsesIfList=stc.get(dhcp_device, "primaryif"),
+    )
+
+    dhcp_block = dhcp_enable["ReturnList"]
+
+    stc.config(
+        dhcp_block, CircuitId="NAFISA-TEST 1/1/1/@x(122,1122)", EnableCircuitId=True
+    )
+
+    print("Connecting to chassis")
+    stc.perform("AttachPorts")
     stc.apply()
 
-    xdata = np.arange(10)
-    ydata = []
+    print("Start DHCP bind")
+    stc.perform("Dhcpv4Bind", blockList=dhcp_block)
 
-    stc.perform("Dhcpv4BindCommand", blockList=dhcp_block)
+    print("Wait for DHCP bind")
+    stc.perform("Dhcpv4BindWait", ObjectList=port1, WaitTime=100)
 
-    polling.poll(
-        lambda: stc.get(dhcp_block, "BlockState") == "BOUND",
-        step=1,
-        poll_forever=True,
-    )
+    if stc.get(dhcp_block, "BlockState") != "BOUND":
+        exit()
 
-    print(f"State is now: {stc.get(dhcp_block, 'BlockState')}")
+    stc.perform("Dhcpv4Release", blockList=dhcp_block)
+    stc.perform("Dhcpv4ReleaseWait", ObjectList=port1, WaitTime=100)
 
-    stc.perform("Dhcpv4ReleaseCommand", blockList=dhcp_block)
-
-    polling.poll(
-        lambda: stc.get(dhcp_block, "BlockState") == "IDLE",
-        step=1,
-        poll_forever=True,
-    )
-
-    print(f"State is now: {stc.get(dhcp_block, 'BlockState')}")
     results = stc.get(dhcp_block, "children-dhcpv4blockresults")
     print(f"Bind rate {stc.get(results, 'BindRate')} sessions/sec")
-
-    # for i in range(3):
-    #     # dhcp = stc.perform("Dhcpv4BindCommand", blockList=dhcp_block)
-    #     print("waiting for bind")
-    #     # stc.perform("SequencerStepCommand")
-    #     # stc.perform("SequencerStepCommand")
-    #     # stc.perform("SequencerPauseCommand")
-    #     # time.sleep(5)
-    #     stc.perform("SequencerStartCommand")
-    #     print("starting release")
-
-    #     # stc.perform("Dhcpv4ReleaseCommand", blockList=dhcp_block)
-    #     # time.sleep(5)
-    #     # stc.perform("SequencerStepCommand")
-    #     # stc.perform("SequencerStepCommand")
-    #     # stc.perform("SequencerPauseCommand")
-    #     # stc.perform("SequencerStopCommand")
-    #     stc.wait_until_complete()
-    #     results = stc.get(dhcp_results["ReturnedDataSet"], "ResultHandleList")
-    #     # ydata.append(stc.get(results, "BindRate"))
-    #     print("Bind rate: {}".format(stc.get(results, "BindRate")))
-
-    # Plot the data
-    # plt.xlabel("Iteration #")
-    # plt.ylabel("Bind Rate (sessions/sec)")
-    # plt.axis(([1, 10, 0, 70]))
-    # plt.plot(xdata, np.array(ydata, dtype="float32"))
-    # plt.show()
-
-    stc.disconnect(chassis_ip)
-    stc.delete(deviceList)
-    stc.delete(port_handle)
-    stc.delete(project)
-    stc.end_session()
 
 
 if __name__ == "__main__":
