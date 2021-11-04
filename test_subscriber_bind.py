c51ea773991209d4bf2017c276e0b685a1922710
Nafisa Tabassum <nafisa.tabassum@telus.com>
2021-11-04 11:40:22 -0600

    renamed to follow convention

--- test_subscriber_bind.py
+++ test_subscriber_bind.py
@@ -1,7 +1,27 @@
-"""Subscriber bind confirmations on RE01"""
+"""
+    TC:1.1 - Basic Subscriber Bind Confirmation
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This test is to verify whether a subscriber or set of subscribers are bound on an RE.
+
+    Test Steps:
+        Step 1: Use existing subscriber accounts in WSIR
+        Step 2: Create DHCP device on Spirent TestCenter (STC)
+        Step 3: Start the subscriber(s) on STC
+        Step 4: Execute 'show service active-subscriber' command on 7750
+    
+    Result:
+        Pass if list of sessions are outputted from Step 4
+        Fail otherwise
+"""
+
+__author__ = "Nafisa Tabassum"
+__version__ = "1.0.0"
+
 import os
 import unittest
 import re
+import HtmlTestRunner
 from SpirentSLC import SLC
 
 # STC session authentication
@@ -49,24 +69,24 @@ def slc_session():
     return s1
 
 
-class SingleSubTest(unittest.TestCase):
-    def setUp(self) -> None:
+class TestSubscriberBind(unittest.TestCase):
+    def setUp(self):
         self.stc = StcPython()
-        project = self.stc.get("system1", "children-project")
+        self.project = self.stc.get("system1", "children-project")
         resultdict = self.stc.perform(
             "CreateLogicalPorts",
-            ParentList=project,
+            ParentList=self.project,
             CreateClassId="port",
             PhyClassName="Ethernet10GigFiber",
         )
-
         self.port1 = resultdict["ReturnList"]
         self.stc.config(self.port1, location=f"//{chassis_ip}/{port}")
 
+    def test_single_sub_bind(self):
         # Create DHCP device on port
         resultdict = self.stc.perform(
             "DeviceCreate",
-            ParentList=project,
+            ParentList=self.project,
             CreateCount=1,
             DeviceCount=1,
             DeviceType="Host",
@@ -83,23 +103,14 @@ class SingleSubTest(unittest.TestCase):
             ParentList=self.dhcp_device,
             UsesIfList=self.stc.get(self.dhcp_device, "primaryif"),
         )
-
         self.dhcp_block = self.dhcp_enable["ReturnList"]
 
         self.stc.config(
             self.dhcp_block, CircuitId="NAFISA-TEST 1/1/1/122", EnableCircuitId=True
         )
-
         self.stc.perform("AttachPorts")
         self.stc.apply()
 
-    def tearDown(self) -> None:
-        self.stc.perform("Dhcpv4Release", blockList=self.dhcp_block)
-        self.stc.perform("Dhcpv4ReleaseWait", ObjectList=self.port1, WaitTime=100)
-        if self.stc.get(self.dhcp_block, "BlockState") != "IDLE":
-            exit()
-
-    def test_single_sub_bind(self):
         self.stc.perform("Dhcpv4Bind", blockList=self.dhcp_block)
         self.stc.perform("Dhcpv4BindWait", ObjectList=self.port1, WaitTime=100)
 
@@ -117,27 +128,11 @@ class SingleSubTest(unittest.TestCase):
             f"Subscriber {sublines[0]} is not bound",
         )
 
-
-class HundredSubTest(unittest.TestCase):
-    maxDiff = None
-
-    def setUp(self) -> None:
-        self.stc = StcPython()
-        project = self.stc.get("system1", "children-project")
-        resultdict = self.stc.perform(
-            "CreateLogicalPorts",
-            ParentList=project,
-            CreateClassId="port",
-            PhyClassName="Ethernet10GigFiber",
-        )
-
-        self.port1 = resultdict["ReturnList"]
-        self.stc.config(self.port1, location=f"//{chassis_ip}/{port}")
-
+    def test_hundred_sub_bind(self):
         # Create DHCP device on port
         resultdict = self.stc.perform(
             "DeviceCreate",
-            ParentList=project,
+            ParentList=self.project,
             CreateCount=1,
             DeviceCount=100,
             DeviceType="Host",
@@ -154,7 +149,6 @@ class HundredSubTest(unittest.TestCase):
             ParentList=self.dhcp_device,
             UsesIfList=self.stc.get(self.dhcp_device, "primaryif"),
         )
-
         self.dhcp_block = self.dhcp_enable["ReturnList"]
 
         self.stc.config(
@@ -162,18 +156,9 @@ class HundredSubTest(unittest.TestCase):
             CircuitId="NAFISA-TEST 1/1/1/@x(300,400)",
             EnableCircuitId=True,
         )
-
         self.stc.perform("AttachPorts")
         self.stc.apply()
 
-    def tearDown(self) -> None:
-        self.stc.perform("Dhcpv4Release", blockList=self.dhcp_block)
-        self.stc.perform("Dhcpv4ReleaseWait", ObjectList=self.port1, WaitTime=100)
-        if self.stc.get(self.dhcp_block, "BlockState") != "IDLE":
-            exit()
-
-    def test_hundred_sub_bind(self):
-        # Simulates subs created in wsir
         accounts = []
         for i in range(300, 400):
             accounts.append(f"sl_NAFISA-TEST/1/1/1/{str(i)}")
@@ -191,6 +176,61 @@ class HundredSubTest(unittest.TestCase):
 
         self.assertListEqual(accounts, sublines)
 
+    def test_thousand_sub_bind(self):
+        # Create DHCP device on port
+        resultdict = self.stc.perform(
+            "DeviceCreate",
+            ParentList=self.project,
+            CreateCount=1,
+            DeviceCount=1000,
+            DeviceType="Host",
+            IfStack="Ipv4If VlanIf EthIIIf",
+            IfCount="1 1 1",
+            port=self.port1,
+        )
+
+        self.dhcp_device = resultdict["ReturnList"]
+
+        self.dhcp_enable = self.stc.perform(
+            "ProtocolCreate",
+            CreateClassId="Dhcpv4BlockConfig",
+            ParentList=self.dhcp_device,
+            UsesIfList=self.stc.get(self.dhcp_device, "primaryif"),
+        )
+        self.dhcp_block = self.dhcp_enable["ReturnList"]
+
+        self.stc.config(
+            self.dhcp_block,
+            CircuitId="NAFISA-TEST 1/1/1/@x(122,1122)",
+            EnableCircuitId=True,
+        )
+        self.stc.perform("AttachPorts")
+        self.stc.apply()
+
+        accounts = []
+        for i in range(122, 1122):
+            accounts.append(f"sl_NAFISA-TEST/1/1/1/{str(i)}")
+
+        self.stc.perform("Dhcpv4Bind", blockList=self.dhcp_block)
+        self.stc.perform("Dhcpv4BindWait", ObjectList=self.port1, WaitTime=100)
+
+        if self.stc.get(self.dhcp_block, "BlockState") == "BOUND":
+            s1 = slc_session()
+            s1.command("environment no more")
+            response = s1.command(
+                'show service active-subscribers detail | match "Subscriber sl_NAFISA-TEST"'
+            )
+            sublines = re.findall("sl_\S+", response.text)
+            sorted_sublines = sorted(sublines, key=lambda x: int(x[21:]))
+
+        self.assertListEqual(accounts, sorted_sublines)
+
+    def tearDown(self):
+        self.stc.perform("Dhcpv4Release", blockList=self.dhcp_block)
+        self.stc.perform("Dhcpv4ReleaseWait", ObjectList=self.port1, WaitTime=100)
+        if self.stc.get(self.dhcp_block, "BlockState") != "IDLE":
+            exit()
+
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner())
